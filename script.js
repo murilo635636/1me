81db9c1e5ed44965d9964533e98dc10ad3893189
murilo635636 <alves.rosa.murilo@escola.pr.gov.br>
2024-10-23 08:05:54 -0300

    1

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 
+#nome do arquivo:ScriptProcessorNode.js
+function mostra resultado ( )
+   {ver numero 1=10;
+   {ver numero 2=15;
+    ver resultado=numero1+numero2;
+    document.getElementByIo (resul)
